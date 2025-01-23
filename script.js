b2e8a8b6ed4b8fe9487ff2498eab99240c5f9f67
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-23 15:13:44 +0100

    Creata una nuova versione della Soluzione del Compito, universalizzando la funzione cicloAddMembers

--- script.js
+++ script.js
@@ -37,13 +37,16 @@ const teamMembers = [
   }
 ];
 
+
+/*Soluzione*/
+
 const teamContainer = document.querySelector('.team-container')
 
-function cicloAddMembers(){
-  for (let i = 1; i < teamMembers.length; i++) {
-    let element = teamMembers[i]
+function cicloAddMembers( array, posPart , elDom){ //Assegnandogli dei parametri, la funzione diventa universale! Questo mi facilita il fatto di richiamarla nel bonus, senza problemi
+  for (let i = posPart; i < array.length; i++) {
+    let element = array[i]
   
-    teamContainer.innerHTML += `
+    elDom.innerHTML += `
            <div class="d-flex bg-dark col-12 col-md-6 col-lg-3">
                 <figure class="m-0 flex-shrink-0">
                     <img height="100px" src="./${element.img}" alt="${element.name}">
@@ -55,10 +58,32 @@ function cicloAddMembers(){
                 </div>
             </div>
         </div>`
-  }
-}
+  }};
 
-cicloAddMembers();
+cicloAddMembers( teamMembers , 1 , teamContainer);
+
+/*Prima funzione fatta - sostituita con quella sopra
+
+ function cicloAddMembers(){
+   for (let i = 1; i < teamMembers.length; i++) {
+     let element = teamMembers[i]
+  
+     teamContainer.innerHTML += `
+            <div class="d-flex bg-dark col-12 col-md-6 col-lg-3">
+                 <figure class="m-0 flex-shrink-0">
+                     <img height="100px" src="./${element.img}" alt="${element.name}">
+                 </figure>
+                 <div class="ms-3 flex-grow-1 p-2">
+                     <h5 class="text-light mb-1 fs-5 fs-md-4 fs-lg-3">${element.name}</h5>
+                     <span class="text-light mb-1 d-block fs-6 fs-md-5">${element.role}</span>
+                     <span class="text-info fs-6">${element.email}</span>
+                 </div>
+             </div>
+         </div>`
+   }
+ }
+   */
+
 
 
 //////////Bonus =>
@@ -75,24 +100,33 @@ cicloAddMembers();
   const img = document.getElementById('img-profilo').value;
 
   const newMember = {
-      name: name,
-      email: email,
-      role: role,
-      img: img,
-  };
+    name: name,
+    email: email,
+    role: role,
+    img: img,
+};
 
-  teamMembers.push(newMember);
+teamMembers.push(newMember);
 
-  teamContainer.innerHTML += `
-    <div class="d-flex bg-dark col-12 col-md-6 col-lg-3">
-      <figure class="m-0 flex-shrink-0">
-        <img height="100px" src="./${newMember.img}" alt="${newMember.name}">
-      </figure>
-      <div class="ms-3 flex-grow-1 p-2">
-        <h5 class="text-light mb-1 fs-5 fs-md-4 fs-lg-3">${newMember.name}</h5>
-        <span class="text-light mb-1 d-block fs-6 fs-md-5">${newMember.role}</span>
-        <span class="text-info fs-6">${newMember.email}</span>
-      </div>
-    </div>
-  `;
+cicloAddMembers (teamMembers , teamMembers.length-1 , teamContainer)
+
+//risolto un bug per il quale, richiamando la funzione e assegnandogli l'ultima posizione index dell'array, mi clonava coppie di card. Così, con "teamMembers.length-1", mi prenderà SEMPRE e SOLO l'ultimo elemento, anche se gliene aggiunto migliaia in corso d'opera
 });
+
+/* Prima funzione Bonus fatta - sostituita con quella sopra
+
+  // teamContainer.innerHTML += `
+  //   <div class="d-flex bg-dark col-12 col-md-6 col-lg-3">
+  //     <figure class="m-0 flex-shrink-0">
+  //       <img height="100px" src="./${newMember.img}" alt="${newMember.name}">
+  //     </figure>
+  //     <div class="ms-3 flex-grow-1 p-2">
+  //       <h5 class="text-light mb-1 fs-5 fs-md-4 fs-lg-3">${newMember.name}</h5>
+  //       <span class="text-light mb-1 d-block fs-6 fs-md-5">${newMember.role}</span>
+  //       <span class="text-info fs-6">${newMember.email}</span>
+  //     </div>
+  //   </div>
+   `;
+});
+*/
+
